4e66caf8aa832bada5130b721a6f650c152a630b
Saifuddin Mohammad <38083799+yaxis1@users.noreply.github.com>
2021-05-19 01:29:45 +0200

    Update meteo.py

--- meteo.py
+++ meteo.py
@@ -1,22 +1,21 @@
-
-#open_weather = f'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={api}'
-
 import json
-import csv
 import requests
+import psycopg2
 import pandas as pd
+from psycopg2 import connect
+from sqlalchemy import create_engine
+import io
 
 lat = 48.86
 lon = 2.34
 part = "current,minutely,hourly,alerts"
-API_key = ""
-
+API_key = 'fad428a7fe71d3a7f648cba70fa84ca1'
+#"228d035930bf3aa35173b0fb636da8ba"
 
 url = f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API_key}&units=metric"
 
 payload={}
 headers = {}    
-
 response = requests.request("GET", url, headers=headers, data=payload)
 daily_weather = response.json()["daily"]
 
@@ -24,6 +23,20 @@ daily_weather = response.json()["daily"]
 with open('daily_weather.json', 'w') as outfile:
     json.dump(daily_weather, outfile) 
 
-df = pd.read_json (r'daily_weather.json')
-df.to_csv (r'weather.csv', mode = 'a', index = None)
+df = pd.read_json(r'daily_weather.json') #Data frame
+
+engine = create_engine('postgresql+psycopg2://sputnik:starman@127.0.0.1:5432/starlink')
  
+def write_data(df):
+    df.head(0).to_sql(f"meteo", engine, if_exists='append',index=False) #appends daily weather data
+    conn = engine.raw_connection()
+    cur = conn.cursor()
+    output = io.StringIO()
+    df.to_csv(output, sep='\t', header=False, index=False)
+    output.seek(0)
+    contents = output.getvalue()
+    cur.copy_from(output, f'meteo', null="") # null values become ''
+    conn.commit()
+    
+write_data(df)
+

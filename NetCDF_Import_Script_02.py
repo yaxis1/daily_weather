57d83e999b1fb1291057410c744b17c2192c6705
Saifuddin Mohammad <38083799+yaxis1@users.noreply.github.com>
2021-05-25 20:03:01 +0200

    'merging'

--- NetCDF_Import_Script_02.py
+++ NetCDF_Import_Script_02.py
@@ -1,58 +1,153 @@
-import os,io, os.path, itertools, datetime, json, tempfile, gc, psutil
-from netCDF4 import Dataset
+import os, os.path, itertools, datetime, json, tempfile, gc
+
 import numpy as np
 import xarray as xr
 import pandas as pd
 #import rasterio as rio
+import rioxarray as riox
 import subprocess
-
-from sqlalchemy import create_engine
-import psycopg2 
-import time
-
+import psycopg2
 #os.environ["GDAL_DATA"] = os.path.join(os.environ["CONDA_PREFIX"], r"Library/share/gdal")
-start_time = time.time()
 
-db_name = 'aiu_db'
-db_host = '40.89.177.14'
-db_user = 'postgres'
-db_password = 'miarujahhahh'
-db_schema = 'buffer'
-os.environ['PGPASSWORD'] = db_password
 
-class netcdf_cols:
-
-    def __init__(self,path):
-        self.data = Dataset(path)
-        self.time_data = self.data.variables['time'][:]
-        self.longitude_data = self.data.variables['longitude'][:]
-        self.latitude_data = self.data.variables['latitude'][:]
-                
-        self.df_time = pd.DataFrame(self.time_data, columns = ['Time'])
-        self.df_lat = pd.DataFrame(self.latitude_data, columns = ['lat'] )
-        self.df_lon = pd.DataFrame(self.longitude_data, columns = ['lon'] )
+class netcdf:
     
-    def check_scale(self,x):
-        if hasattr(data.variables[str(x)],'scale_factor'):
-            return (data.variables[str(x)].scale_factor)
+    def __init__(self, *args, **kwargs):
+        """
+        Open file
+        """
+        self.dataset = xr.open_dataset(*args, **kwargs)
+        self.is_open = True        
+        
+    
+    def close(self):
+        self.dataset.close()
+        self.is_closed = False
+        
+    
+    @staticmethod
+    def normalize(signal, intr_coords, intr_values):
+        """
+        Combine the signal's name with the values of the intrinsic coordinates for the new, complete signal name
+        Inserts the new signal name (json string) in the table self.signals, and return the corresponding signal_id value
+        """
+        if intr_coords==None:
+            intr_coords = set()
+        if intr_values==None:
+            intr_values = set()
+        
+        if len(intr_coords)!=len(intr_values):
+            raise ValueError()
+        
+        if len(intr_coords)==0:
+            signal_name = json.dumps([signal])
         else:
-            return ('None')
-
-
-    df_meta_data = pd.DataFrame(columns=['Long Name','Units'])
-
-    for x in data.variables.keys(self):
-        df_meta_data = df_meta_data.append({'Long Name' : data.variables[str(x)].long_name, 
-                                            'Units' : str(data.variables[str(x)].units), 
-                                            'scale_factor' : check_scale(x)},  ignore_index = True) 
-
-    engine = create_engine('postgresql+psycopg2://postgres:miarujahhahh@40.89.177.14:5432/aiu_db',connect_args={'options': '-csearch_path={}'.format(dbschema)})
+            signal_name = json.dumps([signal, {c:v.item() for c,v in zip(intr_coords, intr_values)}])
+            
+        return signal_name
     
-    def write_data(self,name,df):
-        df.to_sql(f"netcdf_{name}", engine, if_exists='replace',index=False)
+    
+    def rasterize(self, time="time", latitude="latitude", longitude="longitude", drop_coords=None, num_rasters=1, raster_type="GeoTIFF"):
+        """
+        Generator that yields rasters
+        gis_coords = names of coordinates that contain timestamp/datetime, latitude and longitude
+        drop_coords = redundant or uniquely valued coordinates to drop
+        """
+        
+        #Database connection
+
+        db_name = 'aiu_db'
+        db_host = '40.89.177.14'
+        db_user = 'postgres'
+        db_password = 'miarujahhahh'
+        db_tablename = 'netcdf_rasters'
+        os.environ['PGPASSWORD'] = db_password
+        
+        con = psycopg2.connect(database=db_name, user=db_user, password=db_password, host=db_host, port="5432")
+        cur = con.cursor()
+
+
+        if num_rasters!=None and num_rasters<=0:
+            raise ValueError()
+        
+        gis_coords = {time, latitude, longitude} & set(self.dataset.coords)
+        
+        if drop_coords==None:
+            drop_coords = set()
+        else:
+            drop_coords = set(drop_coords)
+            
+        if not drop_coords<=set(self.dataset.coords):
+            raise KeyError()
+        
+        if gis_coords&drop_coords!=set():
+            raise KeyError()
+            
+        intr_coords = set(self.dataset.coords) - (gis_coords|drop_coords)
+        
+        temp_dir = tempfile.TemporaryDirectory()
+        
+        rasters = []
+
+
+    
+        for t in self.dataset[time].values:
+            for dat in self.dataset.data_vars:
+                for intr_vals in itertools.product(*[self.dataset[intr].values for intr in intr_coords]):
+                    
+                    signal = self.normalize(dat, intr_coords, intr_vals)
+
+                    da = self.dataset[dat].sel(time=t, **dict(zip(intr_coords, intr_vals)))
+
+                    for ic in intr_coords|{time}:
+                        da = da.drop(ic)
+
+                    for dc in drop_coords&set(da.coords):
+                        da = da.reduce(np.nanmin, dim=dc)
+                        
+                    da = da.assign_coords(**{longitude: (da[longitude]+180)%360-180}).sortby(longitude)
+                    
+                    
+                    if raster_type.lower()=="geotiff":
+                        da.rio.set_spatial_dims(longitude, latitude)
+                        da.rio.set_crs("EPSG:4326")
+                        path = os.path.join(temp_dir.name, f"raster_file_{len(rasters)+1}.tif")
+                        da.rio.to_raster(path)
+                        
+                    elif raster_type.lower()=="netcdf":
+                        path = os.path.join(temp_dir.name, f"raster_file_{len(rasters)+1}.nc")
+                        da.to_netcdf(path)
+                        
+                    rasters.append( (t, signal, path) )
+                    
+                    if num_rasters!=None and len(rasters)>=num_rasters:
+                        check_table=cur.execute("SELECT EXISTS (SELECT FROM information_schema.tables WHERE  table_schema = 'public' AND table_name= str({}));".format(db_tablename)) 
+                        con.commit()
+                        if not check_table:
+
+                            cmd = 'raster2pgsql -s 4326 -C -F -t auto {}* {} | psql -U {} -d {} -h {} -p 5432'.format(path,db_tablename,db_user,db_name,db_host)
+                            yield rasters
+                        #if not check_table(db_tablename):
+                        #try:
+                            subprocess.call(cmd, shell=True)
+                       # except subprocess.CalledProcessError:
+                       #     return('Table exists')
+
+                        else:
+                            print('table exists')
+                        rasters.clear()
+            
+        if len(rasters)>0:
+#            cmd = 'raster2pgsql -s 4326 -C -F -t path*.tif netcdf_raster | psql -U {} -d {} -h {} -p 5432'.format(db_user,db_name,db_host)
+            yield rasters
+#            subprocess.call(cmd, shell=True)
+            rasters.clear()
+
+        
+data = netcdf('/mnt/data/rasters/WindData2020_03_13_to_26.nc')
+
+#for x in data.rasterize():
+#    print(x)
  
-
-
-dataset = netcdf_cols('WindData2020_03_13_to_26.nc')
-
-print(dataset.data)
+for x in data.rasterize():
+    print(x)
